215004a9e89aca3fd406e1201d3b1df47ecff663
lupasdanielknigts <162113330+lupasdanielknigts@users.noreply.github.com>
2024-03-05 18:51:49 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output:'export',
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
-/* module.exports = nextConfig*/
-export default nextConfig;
+
+module.exports = nextConfig;
